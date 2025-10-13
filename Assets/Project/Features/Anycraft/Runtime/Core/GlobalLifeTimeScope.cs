1937afe076c453b3f4df276b3715d2ee2009b512
irudichph <irudichph@gmail.com>
2025-10-13 10:34:53 +0300

    [main]

---
+++ Assets/Project/Features/Anycraft/Runtime/Core/GlobalLifeTimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Anycrat.Core
+{
+    public sealed class GlobalLifeTimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
