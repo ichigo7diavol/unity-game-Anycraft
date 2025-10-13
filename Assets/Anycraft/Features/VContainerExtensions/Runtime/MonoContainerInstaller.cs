5e3ebd8a1de965c933d1e6d50b1f2e538cf3f671
irudichph <irudichph@gmail.com>
2025-10-13 11:36:04 +0300

    [main] fix

--- Assets/Anycraft/Features/VContainerExtensions/Runtime/MonoContainerInstaller.cs
+++ Assets/Anycraft/Features/VContainerExtensions/Runtime/MonoContainerInstaller.cs
@@ -1,11 +1,12 @@
 using Anycraft.Features.Validation;
 using JetBrains.Annotations;
+using UnityEngine;
 using VContainer;
 
 namespace Anycraft.Features.VContainerExtenions
 {
     [UsedImplicitly]
-    public abstract class MonoContainerInstaller : IContainerInstaller, IValidatable
+    public abstract class MonoContainerInstaller : MonoBehaviour, IContainerInstaller, IValidatable
     {
         public virtual void Install(IContainerBuilder builder)
         {
