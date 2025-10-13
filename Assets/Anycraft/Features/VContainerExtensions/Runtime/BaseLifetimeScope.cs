9e20d70f1e7193e1a3b0f77f5f1ca2ac714c5670
irudichph <irudichph@gmail.com>
2025-10-13 11:35:28 +0300

    [main] installers

--- Assets/Anycraft/Features/VContainerExtensions/Runtime/BaseLifetimeScope.cs
+++ Assets/Anycraft/Features/VContainerExtensions/Runtime/BaseLifetimeScope.cs
@@ -1,12 +1,33 @@
 using VContainer;
 using VContainer.Unity;
+using UnityEngine;
+using System.Collections.Generic;
+using Anycraft.Features.Validation;
 
 namespace Anycraft.Features.VContainerExtenions
 {
-    public class BaseLifetimeScope : LifetimeScope
+    public class BaseLifetimeScope : LifetimeScope, IValidatable
     {
+        [SerializeField] private List<MonoContainerInstaller> _monoInstallers = new();
+        [SerializeField] private List<ScriptableContainerInstaller> _scriptableInstallers = new();
+
+        public bool Validate(ref string errorMessage)
+        {
+            return true; 
+        }
+
         protected override void Configure(IContainerBuilder builder)
         {
+            foreach (var installer in _monoInstallers)
+            {
+                installer.ValidateAndThrow();
+                installer.Install(builder);
+            }
+            foreach (var installer in _scriptableInstallers)
+            {
+                installer.ValidateAndThrow();
+                installer.Install(builder);
+            }
         }
     }
 }
