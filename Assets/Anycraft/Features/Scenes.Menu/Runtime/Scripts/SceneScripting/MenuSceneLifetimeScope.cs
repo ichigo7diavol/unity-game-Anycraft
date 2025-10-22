7aead3a269726859f654e4ad206515092c776c3a
irudichph <irudichph@gmail.com>
2025-10-22 21:02:02 +0300

    [main] menu prefab

---
+++ Assets/Anycraft/Features/Scenes.Menu/Runtime/Scripts/SceneScripting/MenuSceneLifetimeScope.cs
@@ -0,0 +1,9 @@
+using Anycraft.Features.Extenions.VContainer;
+
+namespace Anycraft.Features.Scenes.SceneMenu
+{
+    public class MenuSceneLifetimeScope
+        : BaseLifetimeScope
+    {
+    }
+}
